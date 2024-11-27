1f1532062ad83059577fcd4067c4b17b23763dda
elhanannw <elhananwong@gmail.com>
2024-11-27 13:46:15 +0800

    Update Transformer.java

--- 2030s/ex4-elhanannw-master/Transformer.java
+++ 2030s/ex4-elhanannw-master/Transformer.java
@@ -2,7 +2,7 @@
  * The Transformer interface that can transform a type T
  * to type U.
  *
- * @author A0308536B Elhanan Neriah Wong
+ * @author elhanannw
  * @version CS2030S AY24/25 Semester 1
  */
 public interface Transformer<T, U> {
