5f68c5d4d6cc0651aa6834e88516044fd7db82ae
elhanannw <elhananwong@gmail.com>
2024-11-27 13:45:16 +0800

    Update JackInTheBox.java

--- 2030s/ex4-elhanannw-master/JackInTheBox.java
+++ 2030s/ex4-elhanannw-master/JackInTheBox.java
@@ -2,7 +2,7 @@
  * This class implements a Jack in the Box Transformer.
  * Takes in an item and returns an item in Some container.
  *
- * @author A0308536B Elhanan Neriah Wong
+ * @author elhanannw
  * @version CS2030S AY24/25 Semester 1
  */ 
 public class JackInTheBox<T> implements Transformer<T, Some<T>> {
