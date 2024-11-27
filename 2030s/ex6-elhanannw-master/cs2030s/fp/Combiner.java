953c9594d2b6d2a4653f948ba3cb5f45df3cf079
elhanannw <elhananwong@gmail.com>
2024-11-27 13:35:45 +0800

    Refactor

--- 2030s/ex6-elhanannw-master/cs2030s/fp/Combiner.java
+++ 2030s/ex6-elhanannw-master/cs2030s/fp/Combiner.java
@@ -2,7 +2,7 @@
  * The Combiner interface that can combine
  * two types S and T to type R.
  *
- * @author A0308536B Elhanan Neriah Wong
+ * @author elhanannw
  * @version CS2030S AY24/25 Semester 1
  */
 
